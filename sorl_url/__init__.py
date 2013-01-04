225fed3c928b8c36707a535f6e3ed8cf7495c4fd
Ben Slavin <ben@bolsterlabs.com>
2013-01-04 08:52:34 -0500

    Adding celery task integration; bumping version to 0.2

--- sorl_url/__init__.py
+++ sorl_url/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '0.1.8'
+__version__ = '0.2'
 VERSION = tuple(map(int, __version__.split('.')))
--- sorl_url/signals.py
+++ sorl_url/signals.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.dispatch import receiver, Signal
 from django.db.models.signals import post_save
+from django.db.models.loading import get_model
 from sorl_url.utils import THUMBNAIL_OPTIONS
 
 precache_sorl = Signal(providing_args=['instance', 'config'])
+
 
 @receiver(post_save)
 def spawn_pre_cache_signal(sender, instance, created, **kwargs):
@@ -13,7 +15,9 @@ def spawn_pre_cache_signal(sender, instance, created, **kwargs):
             precache_sorl.send(sender=sender, instance=instance,
                 model_config=config)
 
+
 def handle_precache(sender, instance, model_config, **kwargs):
+    generated = []
     try:
         all_fields = model_config.keys()
         model_options = model_config.options
@@ -25,12 +29,41 @@ def handle_precache(sender, instance, model_config, **kwargs):
             for field_name in fields:
                 file_field = getattr(instance, field_name)
                 thumbnail = backend.get_thumbnail(file_field, geometry, **options)
-    except Exception, e:
+                generated.append(thumbnail)
+    except:
         if not kwargs.get('fail_silently', True):
             raise
+    return generated
 
+
+try:
+    import celery
+except ImportError:
+    celery = None
+
+if celery:
+    # We put this at this module level so a developer can gain
+    # access even if SORL_URL_PRECACHE != 'celery'
+    @celery.task(ignore_result=True)
+    def handle_precache_task(sender_model, instance_model, instance_pk,
+        model_config, **kwargs):
+        sender = get_model(*sender_model.split('.'))
+        instance = get_model(*instance_model.split('.'))\
+            .objects.get(pk=instance_pk)
+        handle_precache(sender, instance, model_config, **kwargs)
+
+    def delay_precache(sender, instance, model_config, **kwargs):
+        sender_model = "%s.%s" % (sender._meta.app_label,
+            sender._meta.object_name)
+        instance_model = "%s.%s" % (instance._meta.app_label,
+            instance._meta.object_name)
+        handle_precache_task.delay(sender_model, instance_model,
+            model_config, **kwargs)
 
 # A developer may choose to use a method other than Django signals
-# (e.g. Celery) to handle the precache actions.
-if getattr(settings, 'SORL_URL_PRECACHE', True):
+# (e.g. non-Celery task queue) to handle the precache actions.
+_precache = getattr(settings, 'SORL_URL_PRECACHE', True)
+if celery and _precache == "celery":
+    precache_sorl.connect(delay_precache)
+elif _precache:
     precache_sorl.connect(handle_precache)
